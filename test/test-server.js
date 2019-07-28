ac94fbeeb4deadbc161af9e3a59f5e45e3512dca
Christopher <chrisformey@yahoo.com>
2019-07-27 21:57:57 -0500

    add test functions

---
+++ test/test-server.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const expect = chai.expect
+
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+const seedBlogPostData = function () {
+    console.info('seeding blog post data')
+    const seedData = []
+
+    for (let i=1; i<=10; i++) {
+        seedData.push(generateBlogPostData());
+      }
+
+      return BlogPost.insertMany(seedData);
+}
+
+const generateBlogPostData = function () {
+    return {
+        author: {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName()
+          },
+          title: faker.random.words(),
+          content: faker.random.words(),
+          created:  Date.now
+    }
+}
+
+const tearDownDb = function () {
+    console.warn('Deleting database');
+    return mongoose.connection.dropDatabase();
+  }
+
